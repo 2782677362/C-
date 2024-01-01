6f8d67d702487818751a82d015816832bf6899cb
2782677362 <147299781+2782677362@users.noreply.github.com>
2024-01-01 22:17:53 +0800

    Update 2024-1-1.c

--- 2024-1-1.c
+++ 2024-1-1.c
@@ -49,20 +49,75 @@
 
 
 
-char* my_strcpy(char* arr2,char* arr1)
-{
-	assert(arr1 && arr2);
-	char* p = arr2;
-	while (*(arr2++) = *(arr1++))
-		;
-	return p;
-}
+//char* my_strcpy(char* arr2,char* arr1)
+//{
+//	assert(arr1 && arr2);
+//	char* p = arr2;
+//	while (*(arr2++) = *(arr1++))
+//		;
+//	return p;
+//}
+//
+//int main()
+//{	
+//	char arr1[] = "abcdefg";
+//	char arr2[20] = { 0 };
+//	my_strcpy(arr2, arr1);
+//	printf("%s\n", arr2);
+//	return 0;
+//}
 
+
+
+//牛客网圣诞树(BC114)
+//https://www.nowcoder.com/practice/9a03096ed8ab449e9b10b0466de29eb2?tpId=290&tqId=2337396&ru=/exam/oj&qru=/ta/beginner-programmers/question-ranking&sourceUrl=%2Fexam%2Foj
 int main()
-{	
-	char arr1[] = "abcdefg";
-	char arr2[20] = { 0 };
-	my_strcpy(arr2, arr1);
-	printf("%s\n", arr2);
-	return 0;
+{
+    int i = 0;
+    int j = 0;
+    int h = 0;
+    int count = 0;
+    scanf("%d", &h);
+    for (i = 0; i < 3 * h; i++)
+    {
+        if (i % 3 == 0)
+            count++;
+        for (j = 0; j < 3 * h - 1 - i; j++)
+        {
+            printf(" ");
+        }
+        if (i % 3 == 0)
+        {
+            for (j = 0; j < count; j++)
+            {
+                printf("*     ");
+            }
+            printf("\n");
+        }
+        else if (i % 3 == 1)
+        {
+            for (j = 0; j < count; j++)
+            {
+                printf("* *   ");
+            }
+            printf("\n");
+        }
+        else
+        {
+            for (j = 0; j < count; j++)
+            {
+                printf("* * * ");
+            }
+            printf("\n");
+        }
+    }
+    for (i = 0; i < h; i++)
+    {
+        for (j = 0; j < 3 * h - 1; j++)
+        {
+            printf(" ");
+        }
+        printf("*\n");
+    }
+    return 0;
 }
